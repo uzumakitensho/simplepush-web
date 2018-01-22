cab0d6c33d0f930229aa7993a8e32935b17c2d4b
uzumakitensho <afridianhafid@gmail.com>
2018-01-22 18:16:44 +0700

    first commit

---
+++ Config.php
@@ -0,0 +1,8 @@
+<?php 
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
+define('DB_NAME','simple_push_notif');
+define('DB_HOST','localhost');
+
+//defined a new constant for firebase api key
+define('FIREBASE_API_KEY', 'AAAAcEk0cKM:APA91bGWpn9vIcSuSmgo_zZxrqSYowtcauqPp_XZXlXIfWdbDuLN-dEsq_JNEyDItYzzW0Q7WzBHbCRUBcUqf6-ujuJxoGlVf3ArJy_UP3YoZFIx9phi1jDdAs71445HI_aYS230rY7_');
